45c46e83905b353f6c3e4f6022171f011bc1573b
Adam Bulgatz <adam@bulgatz.com>
2015-02-09 19:37:49 -0600

    Added correct code repository

--- chef/cookbooks/cocoon/recipes/clone_project.rb
+++ chef/cookbooks/cocoon/recipes/clone_project.rb
@@ -8,13 +8,14 @@
 # Clones the rails project
 #
 
-directory "/home/vagrant/code" do
-  recursive true
-  action :delete
-end
+# Erase everything in the code folder
+# directory "/home/vagrant/code" do
+#   recursive true
+#   action :delete
+# end
 
-git "/home/vagrant/code" do
-	repository "git@github.com:railstutorial/sample_app.git"
+git "/home/vagrant/code/igp" do
+	repository "git@github.com:abulgatz/cs499igp.git"
 	reference "master"
 	action :checkout
 	# user "vagrant"
